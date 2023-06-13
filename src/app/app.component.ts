7e29f3e126e6263cc7113de8f1f0b0cacaecc297
Jozuan Lopez <jozuanlopez@gmail.com>
2023-06-12 21:01:41 -0600

    Se cambió el nombre del negocio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public businessName: string = 'El Manzano Weno';
+  public businessName: string = 'Librería La Joya';
 }
