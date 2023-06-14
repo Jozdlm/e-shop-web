955147cdc5b96e4b25904215d7e900eb8db7cbca
Jozuan Lopez <jozuanlopez@gmail.com>
2023-06-13 20:27:25 -0600

    Generado el componente Item Cart

---
+++ src/app/store/components/item-cart/item-cart.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-item-cart',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './item-cart.component.html',
+  styleUrls: ['./item-cart.component.css']
+})
+export class ItemCartComponent {
+
+}
