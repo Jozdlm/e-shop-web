9d19eafbaf2e88ef98ee5fb4c80a35feece190e5
Jozuan Lopez <jozuanlopez@gmail.com>
2023-06-13 21:04:12 -0600

    Definido metodo para eliminar un item del carrito en el componente

--- src/app/store/components/item-cart/item-cart.component.ts
+++ src/app/store/components/item-cart/item-cart.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ICartItem } from '../../interfaces/cart-item';
+import { ShoppingCartService } from '../../services/shopping-cart.service';
 
 @Component({
   selector: 'app-item-cart',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './item-cart.component.html',
-  styleUrls: ['./item-cart.component.css']
+  styleUrls: ['./item-cart.component.css'],
 })
 export class ItemCartComponent {
+  private _cartService = inject(ShoppingCartService);
 
+  @Input({ alias: 'cartItem', required: true })
+  public item!: ICartItem;
+
+  public removeItem(productId: number): void {
+    this._cartService.removeFromCart(productId);
+  }
 }
