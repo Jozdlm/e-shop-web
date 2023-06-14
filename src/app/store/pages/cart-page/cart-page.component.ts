ff6f315c191b9fada24ce86b24893beddeb6606a
Jozuan Lopez <jozuanlopez@gmail.com>
2023-06-13 21:50:41 -0600

    Se ha dividido el componente en uno más pequeño y reutilizable

--- src/app/store/pages/cart-page/cart-page.component.ts
+++ src/app/store/pages/cart-page/cart-page.component.ts
@@ -2,14 +2,15 @@ import { Component, computed, inject } from '@angular/core';
 import { ShoppingCartService } from '../../services/shopping-cart.service';
 import { ICartItem } from '../../interfaces/cart-item';
 import { IProduct } from '../../interfaces/product';
-import { NgIf, NgFor, CurrencyPipe } from '@angular/common';
+import { CommonModule } from '@angular/common';
+import { ItemCartComponent } from '../../components/item-cart/item-cart.component';
 
 @Component({
   selector: 'app-cart-page',
   templateUrl: './cart-page.component.html',
   styleUrls: ['./cart-page.component.css'],
   standalone: true,
-  imports: [NgIf, NgFor, CurrencyPipe],
+  imports: [CommonModule, ItemCartComponent],
 })
 export class CartPageComponent {
   private _shoppingCartService = inject(ShoppingCartService);
@@ -24,17 +25,4 @@ export class CartPageComponent {
   });
 
   constructor() {}
-
-  public increaseQty(product: IProduct): void {
-    this._shoppingCartService.addToCart(product);
-  }
-
-  public decreaseQty(cartItem: ICartItem): void {
-    const { product, quantity } = cartItem;
-    this._shoppingCartService.decreaseQuantity(product.id, quantity);
-  }
-
-  public removeFromCart(productId: number): void {
-    this._shoppingCartService.removeFromCart(productId);
-  }
 }
