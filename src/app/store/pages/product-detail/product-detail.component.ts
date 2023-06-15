659da1cb0df476a3858be1d5842b75fd667518e3
Jozuan Lopez <jozuanlopez@gmail.com>
2023-06-14 20:22:14 -0600

    Generado componente para los detalles de un producto

---
+++ src/app/store/pages/product-detail/product-detail.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-product-detail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent {
+
+}
