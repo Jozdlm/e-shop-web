9695e380f4b201a56aa1d75c6db6a2f73dc72a91
Jozuan Lopez <jozuanlopez@gmail.com>
2023-06-14 21:27:00 -0600

    Se obtiene el product id de la ruta mediante el input

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
-import { Routes } from "@angular/router";
-import { CartPageComponent } from "./store/pages/cart-page/cart-page.component";
-import { HomePageComponent } from "./store/pages/home-page/home-page.component";
+import { Routes } from '@angular/router';
+import { CartPageComponent } from './store/pages/cart-page/cart-page.component';
+import { HomePageComponent } from './store/pages/home-page/home-page.component';
 
 export const APP_ROUTES: Routes = [
   { path: '', component: HomePageComponent, pathMatch: 'full' },
   { path: 'shopping-cart', component: CartPageComponent },
+  {
+    path: 'product/:id',
+    loadComponent: () =>
+      import('./store/pages/product-detail/product-detail.component').then(
+        (c) => c.ProductDetailComponent
+      ), 
+  },
   { path: '**', redirectTo: '' },
 ];
--- src/app/store/pages/product-detail/product-detail.component.ts
+++ src/app/store/pages/product-detail/product-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,5 +9,6 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent {
-
+  @Input()
+  public id!: number;
 }
