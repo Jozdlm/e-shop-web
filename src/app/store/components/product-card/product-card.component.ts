c6fe6fd4a18d8c4b05b3bab80a4772aff73d89f7
Jozuan Lopez <jozuanlopez@gmail.com>
2023-06-14 21:15:15 -0600

    Se han remplazado varias clases css por clases de Tailwind

--- src/app/store/components/product-card/product-card.component.ts
+++ src/app/store/components/product-card/product-card.component.ts
@@ -1,14 +1,15 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { IProduct } from '../../interfaces/product';
 import { ShoppingCartService } from '../../services/shopping-cart.service';
-import { CurrencyPipe } from '@angular/common';
+import { CommonModule, CurrencyPipe } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.css'],
   standalone: true,
-  imports: [CurrencyPipe],
+  imports: [CommonModule, RouterModule],
 })
 export class ProductCardComponent implements OnInit {
   @Input() product!: IProduct;
