309e76c4e3a09a8948bcecc47fddb0f8fe426148
Jozuan Lopez <jozuanlopez@gmail.com>
2023-06-16 22:08:10 -0600

    Signal para la cantidad, metodos para incrementar o decrementar el estado

--- src/app/store/pages/product-detail/product-detail.component.ts
+++ src/app/store/pages/product-detail/product-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, inject } from '@angular/core';
+import { Component, Input, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IProduct } from '../../interfaces/product';
 import { ProductsService } from '../../services/products.service';
@@ -8,17 +8,29 @@ import { ButtonComponent } from 'src/app/common/components/button/button.compone
   selector: 'app-product-detail',
   standalone: true,
   imports: [CommonModule, ButtonComponent],
-  templateUrl: './product-detail.component.html',
-  styles: []
+  templateUrl: './product-detail.component.html'
 })
 export class ProductDetailComponent {
   private readonly _productService = inject(ProductsService);
 
   public product: IProduct | undefined = undefined;
+  public quantity = signal<number>(1);
 
   @Input()
   public set id(productId: number) {
     this._productService.getProductById(productId)
       .subscribe(product => this.product = product);
   };
+
+  constructor() {}
+
+  public increaseQty(): void {
+    this.quantity.update(value => value + 1);
+  }
+
+  public decreaseQty(): void {
+    this.quantity.update(value => 
+      (this.quantity() == 1) ? 1 : value - 1  
+    );
+  }
 }
