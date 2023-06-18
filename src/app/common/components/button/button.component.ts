5652687cfc5704e8a74df6cc65f30491291981a9
Jozuan Lopez <jozuanlopez@gmail.com>
2023-06-17 21:52:43 -0600

    Boton reutilizable estilizado con Tailwind CSS

--- src/app/common/components/button/button.component.ts
+++ src/app/common/components/button/button.component.ts
@@ -7,12 +7,14 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   template: `
     <button
-      class="py-2.5 px-4 rounded border border-gray-300 hover:bg-green-500/10 hover:border-green-800"
+      class="py-2.5 px-4 rounded bg-green-700 text-white hover:bg-green-800"
     >
-      <span class="text-sm font-semibold text-green-800">
+      <span class="text-sm font-semibold">
         <ng-content></ng-content>
       </span>
     </button>
   `,
 })
-export class ButtonComponent {}
+export class ButtonComponent {
+  public outlineClass = 'border border-gray-300 hover:bg-green-500/10 hover:border-green-800 text-green-800'
+}
