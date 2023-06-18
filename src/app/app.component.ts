bfdf61789f1894cc308212aac60c2ecbd1d0ac70
Jozuan Lopez <jozuanlopez@gmail.com>
2023-06-17 21:47:41 -0600

    Se obtiene el nombre del negocio dinámicamente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +11,19 @@ import { NavbarComponent } from './shared/navbar/navbar.component';
   imports: [RouterOutlet, NavbarComponent],
 })
 export class AppComponent {
-  public businessName: string = 'El Huerto Verde';
+  private _http = inject(HttpClient);
+
+  public businessName: string = '';
+
+  constructor() {
+    this._http
+      .get<IBusiness>('http://localhost:3000/business')
+      .subscribe((business) => {
+        this.businessName = business.name;
+      });
+  }
+}
+
+interface IBusiness {
+  name: string;
 }
