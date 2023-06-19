0b5b7ba919d764b2442a988e047bdf29e88c053b
Jozuan Lopez <jozuanlopez@gmail.com>
2023-06-18 22:41:00 -0600

    Se utiliza el componente reutilizable del dropdown

--- src/app/shared/navbar/cart-dropdown/cart-dropdown.component.ts
+++ src/app/shared/navbar/cart-dropdown/cart-dropdown.component.ts
@@ -4,30 +4,27 @@ import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from 'src/app/common/components/button/button.component';
 import { ProductImageDirective } from 'src/app/common/directives/product-image.directive';
+import { DropdownComponent } from 'src/app/common/components/dropdown/dropdown.component';
 
 @Component({
   selector: 'app-cart-dropdown',
   templateUrl: './cart-dropdown.component.html',
   styles: [],
   standalone: true,
-  imports: [CommonModule, RouterLink, ButtonComponent, ProductImageDirective],
+  imports: [
+    CommonModule,
+    RouterLink,
+    ButtonComponent,
+    ProductImageDirective,
+    DropdownComponent,
+  ],
 })
 export class CartDropdownComponent {
   private _cartService = inject(ShoppingCartService);
-
-  public showMenu: boolean = false;
 
   public cartItems = this._cartService.shoppingCart;
   public cartUnits = this._cartService.cartUnits;
   public cartAmmount = this._cartService.cartAmmount;
 
   constructor() {}
-
-  public displayDropdown(): void {
-    this.showMenu = true;
-  }
-
-  public hideDropdown(): void {
-    this.showMenu = false;
-  }
 }
