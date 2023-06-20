626f3fc5d392fc96aecd7544a9a66688cfc1db90
Jozuan Lopez <jozuanlopez@gmail.com>
2023-06-19 20:32:54 -0600

    Generado el componente my-account y definida su ruta en app.routes

---
+++ src/app/account/pages/my-account/my-account.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './my-account.component.html',
+  styles: [
+  ]
+})
+export class MyAccountComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,7 +10,14 @@ export const APP_ROUTES: Routes = [
     loadComponent: () =>
       import('./store/pages/product-detail/product-detail.component').then(
         (c) => c.ProductDetailComponent
-      ), 
+      ),
+  },
+  {
+    path: 'my-account',
+    loadComponent: () =>
+      import('./account/pages/my-account/my-account.component').then(
+        (c) => c.MyAccountComponent
+      ),
   },
   { path: '**', redirectTo: '' },
 ];
