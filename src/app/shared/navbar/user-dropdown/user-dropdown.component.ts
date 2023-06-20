f60983ed8b887ed31b491474c6abd5805a9bc2dc
Jozuan Lopez <jozuanlopez@gmail.com>
2023-06-19 21:39:16 -0600

    Menu de navegación para user-dropdown

--- src/app/shared/navbar/user-dropdown/user-dropdown.component.ts
+++ src/app/shared/navbar/user-dropdown/user-dropdown.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DropdownComponent } from 'src/app/common/components/dropdown/dropdown.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-user-dropdown',
   standalone: true,
-  imports: [CommonModule, DropdownComponent],
+  imports: [CommonModule, RouterModule, DropdownComponent],
   templateUrl: './user-dropdown.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class UserDropdownComponent {
+  public menuItems: MenuItem[] = [
+    {link: 'my-account', placeholder: 'Configuración cuenta'},
+    {link: 'my-orders', placeholder: 'Mis pedidos'},
+    {link: 'purchase-history', placeholder: 'Historial de compras'},
+  ];
+}
 
+interface MenuItem {
+  link: string;
+  placeholder: string;
 }
