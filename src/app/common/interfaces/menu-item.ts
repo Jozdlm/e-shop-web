6cb9006599fed26655fb0701cbf02d6efe63d41f
Jozuan Lopez <jozuanlopez@gmail.com>
2023-06-20 20:08:16 -0600

    Movida de carpeta la interface IMenuItem e importada en user-dropdown

---
+++ src/app/common/interfaces/menu-item.ts
@@ -0,0 +1,4 @@
+export interface IMenuItem {
+  link: string;
+  placeholder: string;
+}
--- src/app/shared/navbar/user-dropdown/user-dropdown.component.ts
+++ src/app/shared/navbar/user-dropdown/user-dropdown.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DropdownComponent } from 'src/app/common/components/dropdown/dropdown.component';
 import { RouterModule } from '@angular/router';
+import { IMenuItem } from 'src/app/common/interfaces/menu-item';
 
 @Component({
   selector: 'app-user-dropdown',
@@ -11,14 +12,9 @@ import { RouterModule } from '@angular/router';
   styles: [],
 })
 export class UserDropdownComponent {
-  public menuItems: MenuItem[] = [
+  public menuItems: IMenuItem[] = [
     {link: 'my-account', placeholder: 'Configuración cuenta'},
     {link: 'my-orders', placeholder: 'Mis pedidos'},
     {link: 'purchase-history', placeholder: 'Historial de compras'},
   ];
-}
-
-interface MenuItem {
-  link: string;
-  placeholder: string;
 }
