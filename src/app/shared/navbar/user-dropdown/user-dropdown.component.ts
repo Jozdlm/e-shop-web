e564e586d8810639aa6487d6a7328b855edb0438
Jozuan Lopez <jozuanlopez@gmail.com>
2023-06-21 20:10:58 -0600

    Se obtienen los datos del servicio y se muestran en el template

--- src/app/shared/navbar/user-dropdown/user-dropdown.component.ts
+++ src/app/shared/navbar/user-dropdown/user-dropdown.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject, Signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DropdownComponent } from 'src/app/common/components/dropdown/dropdown.component';
 import { RouterModule } from '@angular/router';
 import { IMenuItem } from 'src/app/common/interfaces/menu-item';
+import { UserService } from 'src/app/account/services/user.service';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { IUser } from '../../../account/user';
 
 @Component({
   selector: 'app-user-dropdown',
@@ -12,9 +15,15 @@ import { IMenuItem } from 'src/app/common/interfaces/menu-item';
   styles: [],
 })
 export class UserDropdownComponent {
+  private _userService: UserService = inject(UserService);
+
+  public user: Signal<IUser | undefined> = toSignal<IUser>(
+    this._userService.getUserByEmail('jmprueba@example.com')
+  );
+
   public menuItems: IMenuItem[] = [
-    {link: 'my-account', placeholder: 'Configuración cuenta'},
-    {link: 'my-orders', placeholder: 'Mis pedidos'},
-    {link: 'purchase-history', placeholder: 'Historial de compras'},
+    { link: 'my-account', placeholder: 'Configuración cuenta' },
+    { link: 'my-orders', placeholder: 'Mis pedidos' },
+    { link: 'purchase-history', placeholder: 'Historial de compras' },
   ];
 }
