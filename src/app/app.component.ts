85953493fca425dd0f72884d99f1092d72005cc4
Jozuan Lopez <jozuanlopez@gmail.com>
2023-06-21 20:39:23 -0600

    Estilizado el wrapper utilizando Tailwind CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styles: [],
   standalone: true,
   imports: [RouterOutlet, NavbarComponent],
 })
