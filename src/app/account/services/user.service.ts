8d989a5105adbd22e984a93a519c3b023e0bc69a
Jozuan Lopez <jozuanlopez@gmail.com>
2023-06-21 19:59:56 -0600

    Servicio para obtener un usuario mediante el email

---
+++ src/app/account/services/user.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { IUser } from '../user';
+import { map, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private _http: HttpClient = inject(HttpClient);
+  private _apiUrl: string = 'http://localhost:3000';
+
+  constructor() {}
+
+  public getUserByEmail(email: string): Observable<IUser> {
+    return this._http
+      .get<IUser[]>(`${this._apiUrl}/users`, {
+        params: { email },
+      })
+      .pipe(map((users) => users[0]));
+  }
+}
