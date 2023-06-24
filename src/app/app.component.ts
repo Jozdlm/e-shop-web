076c225d1ca2eeb54146ce861787cfb18667d670
Jozuan Lopez <jozuanlopez@gmail.com>
2023-06-23 22:07:08 -0600

    Generado footer-component que es utilizado por el app-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { HttpClient } from '@angular/common/http';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styles: [],
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
 })
 export class AppComponent {
   private _http = inject(HttpClient);
