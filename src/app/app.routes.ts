723424800623c2a69bf7eda4ed7761f3d6096eec
Jozuan Lopez <jozuanlopez@gmail.com>
2023-06-24 23:21:56 -0600

    Las rutas cart son cargadas de forma perezosa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
-import { CartPageComponent } from './store/pages/cart-page/cart-page.component';
 import { HomePageComponent } from './store/pages/home-page/home-page.component';
 
 export const APP_ROUTES: Routes = [
   { path: '', component: HomePageComponent, pathMatch: 'full' },
-  { path: 'shopping-cart', component: CartPageComponent },
+  { 
+    path: 'cart', 
+    loadChildren: () => import('./cart/cart.routes')
+  },
   {
     path: 'product/:id',
     loadComponent: () =>
---
+++ src/app/cart/cart.routes.ts
@@ -0,0 +1,21 @@
+import { Routes } from '@angular/router';
+import { CartPageComponent } from './pages/cart-page/cart-page.component';
+import { CheckoutPageComponent } from './pages/checkout-page/checkout-page.component';
+
+const CART_ROUTES: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: CartPageComponent,
+  },
+  {
+    path: 'checkout',
+    component: CheckoutPageComponent,
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
+
+export default CART_ROUTES;
