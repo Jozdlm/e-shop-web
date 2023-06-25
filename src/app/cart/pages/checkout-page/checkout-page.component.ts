54ecd224979240d860fdb43ab172d7f668ee08b9
Jozuan Lopez <jozuanlopez@gmail.com>
2023-06-24 23:20:45 -0600

    Generado componente checkout-page en cart/pages

---
+++ src/app/cart/pages/checkout-page/checkout-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './checkout-page.component.html'
+})
+export class CheckoutPageComponent {
+
+}
