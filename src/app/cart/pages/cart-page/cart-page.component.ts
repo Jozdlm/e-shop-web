6596550fe55bb3bd416230a6387f31e89256a215
Jozuan Lopez <jozuanlopez@gmail.com>
2023-06-24 23:20:00 -0600

    cart-page movido a la carpeta cart/pages

--- src/app/cart/pages/cart-page/cart-page.component.ts
+++ src/app/cart/pages/cart-page/cart-page.component.ts
@@ -1,15 +1,15 @@
-import { Component, computed, inject } from '@angular/core';
-import { ShoppingCartService } from '../../services/shopping-cart.service';
-import { ICartItem } from '../../interfaces/cart-item';
+import { Component, inject } from '@angular/core';
+import { ShoppingCartService } from '../../../store/services/shopping-cart.service';
 import { CommonModule } from '@angular/common';
-import { ItemCartComponent } from '../../components/item-cart/item-cart.component';
+import { ItemCartComponent } from '../../../store/components/item-cart/item-cart.component';
 import { ButtonComponent } from 'src/app/common/components/button/button.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-cart-page',
   templateUrl: './cart-page.component.html',
   standalone: true,
-  imports: [CommonModule, ItemCartComponent, ButtonComponent],
+  imports: [CommonModule, ItemCartComponent, ButtonComponent, RouterModule],
 })
 export class CartPageComponent {
   private _cartService = inject(ShoppingCartService);
