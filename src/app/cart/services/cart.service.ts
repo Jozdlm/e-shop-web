9a68e893f500f0f3bf82d7e8587af44082773a0f
Jozuan Lopez <jozuanlopez@gmail.com>
2023-06-26 20:45:10 -0600

    Metodo para guardar, actualizar, crear y obtener por id el shopping-cart

--- src/app/cart/services/cart.service.ts
+++ src/app/cart/services/cart.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { IShoppingCart } from '../cart';
+import { switchMap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -10,4 +11,31 @@ export class CartService {
   private _apiUrl: string = 'http://localhost:3000';
 
   constructor() {}
+
+  public saveShoppingCart(cart: IShoppingCart) {
+    return this.getCartById(cart.id!).pipe(
+      switchMap((cart) => {
+        if (cart.id) {
+          return this.updateCart(cart);
+        } else {
+          return this.createCart(cart);
+        }
+      })
+    );
+  }
+
+  public getCartById(cartId: string) {
+    return this._http.get<IShoppingCart>(`${this._apiUrl}/my_cart/${cartId}`);
+  }
+
+  public createCart(cart: IShoppingCart) {
+    return this._http.post<IShoppingCart>(`${this._apiUrl}/my_cart`, cart);
+  }
+
+  public updateCart(cart: IShoppingCart) {
+    return this._http.put<IShoppingCart>(
+      `${this._apiUrl}/my_cart/${cart.id}`,
+      cart
+    );
+  }
 }
