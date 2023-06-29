574e58d94ee4674383e2bae586cc77a53539ce7f
Jozuan Lopez <jozuanlopez@gmail.com>
2023-06-28 21:27:34 -0600

    Se obtienen los datos del usuario y se muestran en el template como JSON

--- src/app/account/pages/my-account/my-account.component.ts
+++ src/app/account/pages/my-account/my-account.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthService } from 'src/app/auth/services/auth.service';
+import { UserService } from '../../services/user.service';
 
 @Component({
   standalone: true,
   imports: [CommonModule],
   templateUrl: './my-account.component.html',
-  styles: [
-  ]
 })
 export class MyAccountComponent {
+  private _authService: AuthService = inject(AuthService);
+  private _userService: UserService = inject(UserService);
 
+  public userId = this._authService.currentSession().id;
+  public user = toSignal(
+    this._userService.getUserById(this.userId)
+  );
 }
