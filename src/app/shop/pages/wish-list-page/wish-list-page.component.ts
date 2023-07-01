1ffabe90a92186c8cee445986c4f3c6c97bb5c0c
Jozuan Lopez <jozuanlopez@gmail.com>
2023-07-01 09:11:47 -0600

    Muestra cada wish-item utilizando el componente correspondiente

--- src/app/shop/pages/wish-list-page/wish-list-page.component.ts
+++ src/app/shop/pages/wish-list-page/wish-list-page.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { WishListService } from '../../services/wish-list.service';
+import { WishItemComponent } from '../../components/wish-item/wish-item.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, WishItemComponent],
   templateUrl: './wish-list-page.component.html',
 })
-export class WishListPageComponent {}
+export class WishListPageComponent {
+  private _wishService: WishListService = inject(WishListService);
+
+  public wishList = this._wishService.wishList;
+}
