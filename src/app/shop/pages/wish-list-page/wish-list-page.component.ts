f10041c146143633d6d850efa619359e49e60bcc
Jozuan Lopez <jozuanlopez@gmail.com>
2023-06-30 20:05:53 -0600

    Generado el componente wish-list-page

---
+++ src/app/shop/pages/wish-list-page/wish-list-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './wish-list-page.component.html',
+})
+export class WishListPageComponent {}
