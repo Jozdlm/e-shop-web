ee790a67ccd0c6d6ae522d81b859f7d7750b5a4a
Jozuan Lopez <jozuanlopez@gmail.com>
2023-07-01 08:16:45 -0600

    Genera el componente wish-item

---
+++ src/app/shop/components/wish-item/wish-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-wish-item',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './wish-item.component.html'
+})
+export class WishItemComponent {
+
+}
