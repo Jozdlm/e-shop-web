5556fdfaa1dccf8be52d781aa80e22be4ce884e6
Jozuan Lopez <jozuanlopez@gmail.com>
2023-06-30 21:24:38 -0600

    Metodo addToList crea un objeto de tipo IWishItem

--- src/app/shop/services/wish-list.service.ts
+++ src/app/shop/services/wish-list.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+import { v4 as uuid } from 'uuid';
+import { IWishItem } from '../wish-list';
+import { IProduct } from 'src/app/store/interfaces/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WishListService {
+  public wishItems = signal<IWishItem[]>([]);
 
   constructor() { }
+
+  public addToList(product: IProduct): void {
+    const wishItem: IWishItem = {
+      id: uuid(),
+      product_id: product.id,
+      description: product.description,
+      img_url: product.img_url,
+      price: product.price
+    }
+  }
 }
