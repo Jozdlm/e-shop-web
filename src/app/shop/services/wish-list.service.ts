54915f287f60c087c7761681caffdd59f2769974
Jozuan Lopez <jozuanlopez@gmail.com>
2023-07-02 20:18:34 -0600

    Define metodo para actualizar una lista de deseos

--- src/app/shop/services/wish-list.service.ts
+++ src/app/shop/services/wish-list.service.ts
@@ -1,10 +1,11 @@
-import {Injectable, signal, computed, inject} from '@angular/core';
+import { Injectable, signal, computed, inject, effect } from '@angular/core';
 import { v4 as uuid } from 'uuid';
 import { IWishItem, IWishList } from '../wish-list';
 import { IProduct, ProductOption } from 'src/app/store/interfaces/product';
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {environment} from "../../../environments/environment";
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import {AuthService} from "../../auth/services/auth.service";
 
 @Injectable({
   providedIn: 'root',
@@ -12,6 +13,7 @@ import {environment} from "../../../environments/environment";
 export class WishListService {
   private _http: HttpClient = inject(HttpClient);
   private _apiUrl: string = environment.apiUrl;
+  private _authService: AuthService = inject(AuthService);
   private _wishItems = signal<IWishItem[]>([]);
 
   public wishList = computed<IWishList>(() => ({
@@ -22,8 +24,12 @@ export class WishListService {
 
   constructor() {}
 
-  public createWishList(): Observable<IWishList> {
-    return this._http.post<IWishList>(`${this._apiUrl}/my_wishlist`, this.wishList())
+  public createWishList(wishList: IWishList): Observable<IWishList> {
+    return this._http.post<IWishList>(`${this._apiUrl}/my_wishlist`, wishList);
+  }
+
+  public updateWishList(wishId: string, wishList: IWishList): Observable<IWishList> {
+    return this._http.put<IWishList>(`${this._apiUrl}/my_wishlist/${wishId}`, wishList);
   }
 
   public addToWish(product: IProduct, option: ProductOption): void {
