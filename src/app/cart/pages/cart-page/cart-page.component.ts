61226a031e91120e0237c67a6c6a0ec3b7465faa
Jozuan Lopez <jozuanlopez@gmail.com>
2023-07-03 17:55:11 -0600

    Muestra los elementos de la wish-list en el cart-page

--- src/app/cart/pages/cart-page/cart-page.component.ts
+++ src/app/cart/pages/cart-page/cart-page.component.ts
@@ -4,16 +4,27 @@ import { CommonModule } from '@angular/common';
 import { ItemCartComponent } from '../../components/item-cart/item-cart.component';
 import { ButtonComponent } from 'src/app/common/components/button/button.component';
 import { RouterModule } from '@angular/router';
+import { WishListService } from '../../../wish/services/wish-list.service';
+import { WishCartComponent } from '../../components/wish-cart/wish-cart.component';
 
 @Component({
   selector: 'app-cart-page',
   templateUrl: './cart-page.component.html',
   standalone: true,
-  imports: [CommonModule, ItemCartComponent, ButtonComponent, RouterModule],
+  imports: [
+    CommonModule,
+    ItemCartComponent,
+    ButtonComponent,
+    RouterModule,
+    WishCartComponent,
+  ],
 })
 export class CartPageComponent {
-  private _cartService = inject(ShoppingCartService);
+  private _cartService: ShoppingCartService = inject(ShoppingCartService);
+  private _wishService: WishListService = inject(WishListService);
+
   public cart = this._cartService.shoppingCart;
+  public wish = this._wishService.wishList;
 
   constructor() {}
 
