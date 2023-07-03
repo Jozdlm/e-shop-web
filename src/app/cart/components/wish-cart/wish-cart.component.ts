da57b897539587166b3d34580ec86c259ac505b3
Jozuan Lopez <jozuanlopez@gmail.com>
2023-07-03 17:51:38 -0600

    Maqueta el componente wish-cart

--- src/app/cart/components/wish-cart/wish-cart.component.ts
+++ src/app/cart/components/wish-cart/wish-cart.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IWishItem } from '../../../wish/wish-list';
+import { ProductImageDirective } from '../../../common/directives/product-image.directive';
 
 @Component({
   selector: 'app-wish-cart',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ProductImageDirective],
   templateUrl: './wish-cart.component.html',
 })
 export class WishCartComponent {
-
+  @Input() wishItem!: IWishItem;
 }
