7f9ec1db7e2158de5435917f1645cbb066b7584d
Jozuan Lopez <jozuanlopez@gmail.com>
2023-07-03 09:02:00 -0600

    Define el template para cada wish-item

--- src/app/shop/components/wish-item/wish-item.component.ts
+++ src/app/shop/components/wish-item/wish-item.component.ts
@@ -1,12 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IWishItem } from '../../wish-list';
+import { ProductImageDirective } from '../../../common/directives/product-image.directive';
+import { RouterModule } from '@angular/router';
+import { ButtonComponent } from '../../../common/components/button/button.component';
 
 @Component({
   selector: 'app-wish-item',
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './wish-item.component.html'
+  imports: [CommonModule, RouterModule, ProductImageDirective, ButtonComponent],
+  templateUrl: './wish-item.component.html',
 })
 export class WishItemComponent {
   @Input() public wishItem: IWishItem | undefined = undefined;
