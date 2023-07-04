bee06ef6eb05cca98e24f7c1a00e18f498d31cdf
Jozuan Lopez <jozuanlopez@gmail.com>
2023-07-03 18:18:44 -0600

    Mueve un item de la wish-list al shopping-cart desde cart-page

--- src/app/cart/components/wish-cart/wish-cart.component.ts
+++ src/app/cart/components/wish-cart/wish-cart.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IWishItem } from '../../../wish/wish-list';
 import { ProductImageDirective } from '../../../common/directives/product-image.directive';
+import { WishListService } from '../../../wish/services/wish-list.service';
 
 @Component({
   selector: 'app-wish-cart',
@@ -10,5 +11,11 @@ import { ProductImageDirective } from '../../../common/directives/product-image.
   templateUrl: './wish-cart.component.html',
 })
 export class WishCartComponent {
+  private _wishService: WishListService = inject(WishListService);
+
   @Input() wishItem!: IWishItem;
+
+  public moveToCart(item: IWishItem): void {
+    this._wishService.moveToCart(item);
+  }
 }
