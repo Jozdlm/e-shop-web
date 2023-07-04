892c72d8bb683930b218da50cead3f726c76b833
Jozuan Lopez <jozuanlopez@gmail.com>
2023-07-03 22:00:45 -0600

    Muestra un mensaje cordial si el carrito está vacío

--- src/app/cart/components/empty-cart/empty-cart.component.ts
+++ src/app/cart/components/empty-cart/empty-cart.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonComponent } from 'src/app/common/components/button/button.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-empty-cart',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, ButtonComponent],
   templateUrl: './empty-cart.component.html',
 })
-export class EmptyCartComponent {
-
-}
+export class EmptyCartComponent {}
