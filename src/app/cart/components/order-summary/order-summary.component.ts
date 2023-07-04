716c58c09f399ec1512c3a9357d1e27183ded13f
Jozuan Lopez <jozuanlopez@gmail.com>
2023-07-03 21:35:11 -0600

    Genera el componente order-summary

---
+++ src/app/cart/components/order-summary/order-summary.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-order-summary',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './order-summary.component.html',
+})
+export class OrderSummaryComponent {
+
+}
