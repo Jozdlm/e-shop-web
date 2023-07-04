62021ff842292a2746cdd35f2657d7aa1e14e144
Jozuan Lopez <jozuanlopez@gmail.com>
2023-07-03 21:41:42 -0600

    Muestra el resumen de la orden actual

--- src/app/cart/components/order-summary/order-summary.component.ts
+++ src/app/cart/components/order-summary/order-summary.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, Signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IShoppingCart } from '../../cart';
+import { RouterModule } from '@angular/router';
+import { ButtonComponent } from 'src/app/common/components/button/button.component';
 
 @Component({
   selector: 'app-order-summary',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, ButtonComponent],
   templateUrl: './order-summary.component.html',
 })
 export class OrderSummaryComponent {
-
+  @Input({ required: true }) 
+  public cart!: Signal<IShoppingCart>;
 }
