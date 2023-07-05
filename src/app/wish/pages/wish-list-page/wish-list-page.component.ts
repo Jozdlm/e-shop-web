4b6dfc97259c907fa661d911be6697eb0a753db7
Jozuan Lopez <jozuanlopez@gmail.com>
2023-07-04 20:39:18 -0600

    Muestra el mensaje empty-wish cuando la wish-list está vacía

--- src/app/wish/pages/wish-list-page/wish-list-page.component.ts
+++ src/app/wish/pages/wish-list-page/wish-list-page.component.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WishListService } from '../../services/wish-list.service';
 import { WishItemComponent } from '../../components/wish-item/wish-item.component';
+import { EmptyWishComponent } from '../../components/empty-wish/empty-wish.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, WishItemComponent],
+  imports: [CommonModule, WishItemComponent, EmptyWishComponent],
   templateUrl: './wish-list-page.component.html',
 })
 export class WishListPageComponent {
