dcca4dea20591818769deebbb09f2e16e324e1e7
Jozuan Lopez <jozuanlopez@gmail.com>
2023-07-04 20:26:55 -0600

    Genera el componente empty-wish

---
+++ src/app/wish/components/empty-wish/empty-wish.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-empty-wish',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './empty-wish.component.html',
+  styles: [
+  ]
+})
+export class EmptyWishComponent {
+
+}
