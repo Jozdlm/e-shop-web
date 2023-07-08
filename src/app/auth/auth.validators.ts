3946f1b1ce0b90cba04d24e1817c2d34703dbc33
Jozuan Lopez <jozuanlopez@gmail.com>
2023-07-08 17:31:16 -0600

    Define el password-validator que valida si las contraseñas coinciden

--- src/app/auth/auth.validators.ts
+++ src/app/auth/auth.validators.ts
@@ -15,3 +15,20 @@ export const customEmailValidator: ValidatorFn = (control: AbstractControl) => {
 
   return null;
 };
+
+export const passwordMatchValidator: ValidatorFn = (control: AbstractControl) => {
+  const password = control.get('password');
+  const password2 = control.get('repeated_password');
+  
+  const errors = password2?.errors || {};
+
+  if (password?.value === password2?.value) {
+    delete errors['passwordMismatch'];
+    return null;
+  }
+
+  errors['passwordMismatch'] = true;
+  password2?.setErrors(errors);
+
+  return { passwordMismatch: true };
+};
