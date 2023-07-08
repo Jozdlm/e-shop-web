1f3540eab7cb6870a31e8b7334bcc4209eb128da
Jozuan Lopez <jozuanlopez@gmail.com>
2023-07-07 19:19:42 -0600

    Define la configuracion de firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyC3XWNg6D18ZZFcuqTWQGgR7XqEgBXgbT8",
+    authDomain: "e-shop-88e07.firebaseapp.com",
+    projectId: "e-shop-88e07",
+    storageBucket: "e-shop-88e07.appspot.com",
+    messagingSenderId: "586515135586",
+    appId: "1:586515135586:web:7dd0d0b0454713ed75253b"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,15 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000'
+  apiUrl: 'http://localhost:3000',
+  firebaseConfig: {
+    apiKey: "AIzaSyC3XWNg6D18ZZFcuqTWQGgR7XqEgBXgbT8",
+    authDomain: "e-shop-88e07.firebaseapp.com",
+    projectId: "e-shop-88e07",
+    storageBucket: "e-shop-88e07.appspot.com",
+    messagingSenderId: "586515135586",
+    appId: "1:586515135586:web:7dd0d0b0454713ed75253b"
+  }
 };
 
 /*
