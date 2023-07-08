9cc8ccd0eca26ca8e6e69f06efe67bd40e3e7b5b
Jozuan Lopez <jozuanlopez@gmail.com>
2023-07-07 19:07:40 -0600

    Genera el componente signup page

---
+++ src/app/auth/pages/signup-page/signup-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-signup-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './signup-page.component.html',
+  styles: [
+  ]
+})
+export class SignupPageComponent {
+
+}
