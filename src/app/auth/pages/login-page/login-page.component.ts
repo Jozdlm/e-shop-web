ab63ecf8c45a044f64c91a78c6832feb7ebdf2dd
Jozuan Lopez <jozuanlopez@gmail.com>
2023-07-07 19:08:43 -0600

    Genera el componente login-page

---
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './login-page.component.html',
+  styles: [
+  ]
+})
+export class LoginPageComponent {
+
+}
