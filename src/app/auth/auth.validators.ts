353cb4f0768f894d045f9c390ed899d0e4c0c1b5
Jozuan Lopez <jozuanlopez@gmail.com>
2023-07-08 17:32:21 -0600

    Modifica el objeto validation-error del email-validator

--- src/app/auth/auth.validators.ts
+++ src/app/auth/auth.validators.ts
@@ -5,21 +5,18 @@ export const customEmailValidator: ValidatorFn = (control: AbstractControl) => {
   const emailPattern: RegExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
   if (!emailPattern.test(value)) {
-    return {
-      email: {
-        message: 'Email is not valid',
-        actualValue: value,
-      },
-    };
+    return { email: true };
   }
 
   return null;
 };
 
-export const passwordMatchValidator: ValidatorFn = (control: AbstractControl) => {
+export const passwordMatchValidator: ValidatorFn = (
+  control: AbstractControl
+) => {
   const password = control.get('password');
   const password2 = control.get('repeated_password');
-  
+
   const errors = password2?.errors || {};
 
   if (password?.value === password2?.value) {
