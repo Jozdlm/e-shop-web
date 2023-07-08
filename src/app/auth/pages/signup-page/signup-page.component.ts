66880279d5503e0e224c15499c02b52a7f6b73cf
Jozuan Lopez <jozuanlopez@gmail.com>
2023-07-08 10:29:36 -0600

    Define el formulario reactivo para el signup-form

--- src/app/auth/pages/signup-page/signup-page.component.ts
+++ src/app/auth/pages/signup-page/signup-page.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
-  selector: 'app-signup-page',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
   templateUrl: './signup-page.component.html',
-  styles: [
-  ]
 })
 export class SignupPageComponent {
+  private _fb: FormBuilder = inject(FormBuilder);
 
+  public signupForm: FormGroup = this._fb.nonNullable.group({
+    first_name: ['', Validators.required],
+    last_name: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    repeated_password: ['', [Validators.required, Validators.minLength(6)]],
+  });
 }
