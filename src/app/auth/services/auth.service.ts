829ae3ff9147de39abc28eeb93a961bdc6a0bfa1
Jozuan Lopez <jozuanlopez@gmail.com>
2023-07-07 20:49:31 -0600

    Define un signal que está al pendiente a los eventos de login, logout an tokens

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,20 +1,29 @@
-import { Injectable, signal, computed } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { Injectable, signal, computed, inject, effect } from '@angular/core';
 import { ISession } from '../session';
+import { Auth, User, user } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  private _auth: Auth = inject(Auth);
+
   private _currentSession = signal<ISession>({
     id: '1',
     username: 'Jozuan Martinez',
     email: 'jmprueba@example.com',
-    token: 'Zwq123fjxjkl3ñ56j5l43d'
+    token: 'Zwq123fjxjkl3ñ56j5l43d',
   });
+
+  public user$ = user(this._auth);
+  public user = toSignal<User | null>(this.user$);
 
   public currentSession = computed<ISession>(() => {
     return this._currentSession();
-  })
+  });
 
-  constructor() {}
+  constructor() {
+    effect(() => console.log(this.user()));
+  }
 }
