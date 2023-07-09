8bb70e3fae9a0cff7663048bd3b8fd0933e1a008
Jozuan Lopez <jozuanlopez@gmail.com>
2023-07-08 18:00:19 -0600

    Define las distintas rutas del account feature

---
+++ src/app/account/account.routes.ts
@@ -0,0 +1,20 @@
+import { Routes } from '@angular/router';
+import { MyAccountComponent } from './pages/my-account/my-account.component';
+
+const ACCOUNT_ROUTES: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'profile',
+  },
+  {
+    path: 'profile',
+    component: MyAccountComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'profile'
+  }
+];
+
+export default ACCOUNT_ROUTES;
