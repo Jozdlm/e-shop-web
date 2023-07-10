35c4a477aa6e8ffa7d801e320f5cc408f8462a42
Jozuan Lopez <jozuanlopez@gmail.com>
2023-07-09 18:43:29 -0600

    Limpia los valores del signup-form y navega a la ruta inicial

--- src/app/auth/pages/signup-page/signup-page.component.ts
+++ src/app/auth/pages/signup-page/signup-page.component.ts
@@ -1,13 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import {
-  FormBuilder,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ButtonComponent } from 'src/app/common/components/button/button.component';
-import { customEmailValidator, passwordMatchValidator } from '../../auth.validators';
+import {
+  customEmailValidator,
+  passwordMatchValidator,
+} from '../../auth.validators';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
@@ -17,23 +16,31 @@ import { AuthService } from '../../services/auth.service';
 })
 export class SignupPageComponent {
   private _authService: AuthService = inject(AuthService);
+  private _router: Router = inject(Router);
   private _fb: FormBuilder = inject(FormBuilder);
 
-  public signupForm = this._fb.nonNullable.group({
-    first_name: ['', [Validators.required, Validators.minLength(3)]],
-    last_name: ['', [Validators.required, Validators.minLength(3)]],
-    email: ['', [Validators.required, customEmailValidator]],
-    password: ['', [Validators.required, Validators.minLength(6)]],
-    repeated_password: ['', [Validators.required, Validators.minLength(6)]],
-  }, {validators: passwordMatchValidator});
+  public signupForm = this._fb.nonNullable.group(
+    {
+      first_name: ['', [Validators.required, Validators.minLength(3)]],
+      last_name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, customEmailValidator]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      repeated_password: ['', [Validators.required, Validators.minLength(6)]],
+    },
+    { validators: passwordMatchValidator }
+  );
 
   public handleSubmit(): void {
     if (this.signupForm.valid) {
-      const {first_name, last_name, email, password} = this.signupForm.value;
+      const { first_name, last_name, email, password } = this.signupForm.value;
       const fullname = `${first_name} ${last_name}`;
-      
-      this._authService.signup(fullname, email!, password!)
-        .subscribe((value) => console.log('registrado correctamente'));
+
+      this._authService
+        .signup(fullname, email!, password!)
+        .subscribe((_) => console.log('registrado correctamente'));
+
+      this.signupForm.reset();
+      this._router.navigate(['/']);
     }
   }
 }
