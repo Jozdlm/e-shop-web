50e8696155bb1f84ed19f6937fa4670d0acb514a
Jozuan Lopez <jozuanlopez@gmail.com>
2023-07-09 20:18:01 -0600

    Implementa la user-signal del auth-service

--- src/app/shared/navbar/user-dropdown/user-dropdown.component.ts
+++ src/app/shared/navbar/user-dropdown/user-dropdown.component.ts
@@ -1,32 +1,23 @@
-import { Component, inject, Signal } from '@angular/core';
+import { Component, Signal, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DropdownComponent } from 'src/app/common/components/dropdown/dropdown.component';
 import { RouterModule } from '@angular/router';
 import { IMenuItem } from 'src/app/common/interfaces/menu-item';
-import { UserService } from 'src/app/account/services/user.service';
-import { toSignal } from '@angular/core/rxjs-interop';
-import { IUser } from '../../../account/user';
 import { AuthService } from 'src/app/auth/services/auth.service';
+import { User } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-user-dropdown',
   standalone: true,
   imports: [CommonModule, RouterModule, DropdownComponent],
   templateUrl: './user-dropdown.component.html',
-  styles: [],
 })
 export class UserDropdownComponent {
-  private _userService: UserService = inject(UserService);
   private _authService: AuthService = inject(AuthService);
 
-  private _userId: string = this._authService.currentSession().id;
-
-  public user: Signal<IUser | undefined> = toSignal<IUser>(
-    this._userService.getUserById(this._userId)
-  );
+  public user: Signal<User | null | undefined> = this._authService.user;
 
   public baseLink: string = 'account';
-
   public menuItems: IMenuItem[] = [
     { link: 'profile', placeholder: 'Configuración cuenta' },
     { link: 'my-orders', placeholder: 'Mis pedidos' },
