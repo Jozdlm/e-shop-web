238ce0827a33ca224cf9a5f5b448fef9e4bc04c8
Jozuan Lopez <jozuanlopez@gmail.com>
2023-07-11 19:55:08 -0600

    Maqueta el formulario de inicio de sesión de usuarios

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ButtonComponent } from 'src/app/common/components/button/button.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule, ButtonComponent],
   templateUrl: './login-page.component.html',
-  styles: [
-  ]
 })
-export class LoginPageComponent {
-
-}
+export class LoginPageComponent {}
