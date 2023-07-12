7d87d9d2f6bac942a59eb135360275904622e745
Jozuan Lopez <jozuanlopez@gmail.com>
2023-07-11 20:03:03 -0600

    Define el formulario reactivo login-form en login-page

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ButtonComponent } from 'src/app/common/components/button/button.component';
 import { RouterModule } from '@angular/router';
+import { customEmailValidator } from '../../auth.validators';
 
 @Component({
   selector: 'app-login-page',
@@ -10,4 +11,11 @@ import { RouterModule } from '@angular/router';
   imports: [CommonModule, ReactiveFormsModule, RouterModule, ButtonComponent],
   templateUrl: './login-page.component.html',
 })
-export class LoginPageComponent {}
+export class LoginPageComponent {
+  private _fb: FormBuilder = inject(FormBuilder);
+
+  public loginForm = this._fb.nonNullable.group({
+    email: ['', [Validators.required, customEmailValidator]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+}
