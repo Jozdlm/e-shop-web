e5f3f76ccfed21544517ffb752b7768cf745852e
Jozuan Lopez <jozuanlopez@gmail.com>
2023-07-14 16:49:10 -0600

    Remplaza la url de la foto del usuario para definir una por defecto

--- src/app/common/directives/user-photo.directive.ts
+++ src/app/common/directives/user-photo.directive.ts
@@ -1,11 +1,21 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input, inject } from '@angular/core';
 
 @Directive({
   selector: '[userPhoto]',
   standalone: true
 })
 export class UserPhotoDirective {
+  private _elementRef: ElementRef = inject(ElementRef);
 
-  constructor() { }
+  @Input()
+  public set userPhoto(imgUrl: string | undefined | null) {
+    if (imgUrl && imgUrl.trim().length > 1) {
+      this._elementRef.nativeElement.src = imgUrl;
+    } else {
+      this._elementRef.nativeElement.src = 'assets/images/no-user-img.png';
+    }
+  }
+
+  constructor() {}
 
 }
