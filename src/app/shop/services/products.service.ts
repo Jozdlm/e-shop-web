213f769e58347ec8a7d11f9d732a0623d29ebe3b
Jozuan Lopez <jozuanlopez@gmail.com>
2023-07-15 22:30:21 -0600

    Obtiene los productos de Cloud Firestore

--- src/app/shop/services/products.service.ts
+++ src/app/shop/services/products.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject, Signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { IProduct } from 'src/app/store/interfaces/product';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductsService {
+  private _firestore: Firestore = inject(Firestore);
+  private _productsCollection = collection(this._firestore, 'products');
 
-  constructor() { }
+  public products: Signal<IProduct[]> = toSignal(
+    collectionData(this._productsCollection) as Observable<IProduct[]>,
+    {
+      initialValue: [] as IProduct[],
+    }
+  );
+
+  constructor() {}
 }
