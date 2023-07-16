3a474bbbdaa385257907a1a080f5adbbfe3aeae0
Jozuan Lopez <jozuanlopez@gmail.com>
2023-07-15 23:01:51 -0600

    Muestra los productos del product-service en el home-page

--- src/app/shop/pages/home-page/home-page.component.ts
+++ src/app/shop/pages/home-page/home-page.component.ts
@@ -1,24 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, Signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IProduct } from 'src/app/store/interfaces/product';
-import { ProductsService } from 'src/app/store/services/products.service';
 import { ProductCardComponent } from 'src/app/store/components/product-card/product-card.component';
+import { ProductsService } from "src/app/shop/services/products.service";
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styles: [],
   standalone: true,
   imports: [CommonModule, ProductCardComponent],
 })
-export class HomePageComponent implements OnInit {
-  public products: IProduct[] = [];
+export class HomePageComponent {
+  private _productsService: ProductsService = inject(ProductsService);
+  public products: Signal<IProduct[]> = this._productsService.products;
 
-  constructor(private productsService: ProductsService) {}
-
-  ngOnInit(): void {
-    this.productsService.getProducts().subscribe((data) => {
-      this.products = data;
-    });
-  }
+  constructor() {}
 }
