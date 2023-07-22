903569acc6e52e0140bd1b96a901a6ec608942b1
Jozuan Lopez <jozuanlopez@gmail.com>
2023-07-21 19:21:27 -0600

    Elimina la lógica para obtener la información de la empresa del componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component';
-import { HttpClient } from '@angular/common/http';
 import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
@@ -12,19 +11,5 @@ import { FooterComponent } from './shared/footer/footer.component';
   imports: [RouterOutlet, NavbarComponent, FooterComponent],
 })
 export class AppComponent {
-  private _http = inject(HttpClient);
-
-  public businessName: string = '';
-
-  constructor() {
-    this._http
-      .get<IBusiness>('http://localhost:3000/business')
-      .subscribe((business) => {
-        this.businessName = business.name;
-      });
-  }
-}
-
-interface IBusiness {
-  name: string;
+  constructor() {}
 }
