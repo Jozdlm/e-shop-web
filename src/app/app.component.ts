d1dfdbaaac939a514c14752fde4cb918113b705d
Jozuan Lopez <jozuanlopez@gmail.com>
2023-07-22 11:54:11 -0600

    Mueve de carpeta el componente footer al feature shop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component';
-import { FooterComponent } from './shared/footer/footer.component';
+import { FooterComponent } from './shop/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
