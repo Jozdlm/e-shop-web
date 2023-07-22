254975ea465a88d4b12f25a128eff21498cf4608
Jozuan Lopez <jozuanlopez@gmail.com>
2023-07-21 19:22:18 -0600

    Obtiene la información de la empresa utilizando el app-service

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,17 +1,16 @@
-import { Component, Input, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CartDropdownComponent } from './cart-dropdown/cart-dropdown.component';
 import { SearchInputComponent } from './search-input/search-input.component';
 import { CommonModule } from '@angular/common';
-import { ButtonComponent } from 'src/app/common/components/button/button.component';
 import { WishlistDropdownComponent } from './wishlist-dropdown/wishlist-dropdown.component';
 import { RouterModule } from '@angular/router';
 import { UserDropdownComponent } from './user-dropdown/user-dropdown.component';
 import { AuthService } from 'src/app/auth/services/auth.service';
+import { AppService } from 'src/app/app.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styles: [],
   standalone: true,
   imports: [
     CommonModule,
@@ -24,10 +23,13 @@ import { AuthService } from 'src/app/auth/services/auth.service';
 })
 export class NavbarComponent {
   private _authService: AuthService = inject(AuthService);
+  private _appService: AppService = inject(AppService);
 
-  @Input() 
-  public businessName: string = '';
+  public businessName: string  = '';
   public user = this._authService.user;
 
-  constructor() {}
+  constructor() {
+    this._appService.getBusinessInfo()
+      .then(value => this.businessName = value?.name || '');
+  }
 }
