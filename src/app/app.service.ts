3dbd49cf4a99e152d37344ce24468bc7c05cb845
Jozuan Lopez <jozuanlopez@gmail.com>
2023-07-21 19:09:21 -0600

    Define método para obtener la información de la empresa

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { Firestore, doc, getDoc } from '@angular/fire/firestore';
+import { BusinessInfo } from './app';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AppService {
+  private _firestore: Firestore = inject(Firestore);
 
-  constructor() { }
+  constructor() {}
+
+  public async getBusinessInfo(): Promise<BusinessInfo | undefined> {
+    const docRef = doc(this._firestore, 'business', 'info');
+    const docSnap = await getDoc(docRef);
+
+    if (!docSnap.exists()) return;
+
+    return docSnap.data() as BusinessInfo;
+  }
 }
