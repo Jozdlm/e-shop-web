2aa6440f299a15f316436b01fc5a24ce45457b01
Jozuan Lopez <jozuanlopez@gmail.com>
2023-07-23 17:35:26 -0600

    Muestra un mensaje dependiendo de si el usuario está loggeado

--- src/app/wish/components/empty-wish/empty-wish.component.ts
+++ src/app/wish/components/empty-wish/empty-wish.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ButtonComponent } from 'src/app/common/components/button/button.component';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-empty-wish',
@@ -9,4 +10,7 @@ import { ButtonComponent } from 'src/app/common/components/button/button.compone
   imports: [CommonModule, RouterModule, ButtonComponent],
   templateUrl: './empty-wish.component.html',
 })
-export class EmptyWishComponent {}
+export class EmptyWishComponent {
+  private _authService: AuthService = inject(AuthService);
+  public user = this._authService.user;
+}
