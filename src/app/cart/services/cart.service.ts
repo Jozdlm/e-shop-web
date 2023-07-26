3ed0fa98725b8ec2c5d523cf8e1c11c0c1a7ed30
Jozuan Lopez <jozuanlopez@gmail.com>
2023-07-25 21:50:14 -0600

    Define metodo para obtener el carrito de compras de un usuario

--- src/app/cart/services/cart.service.ts
+++ src/app/cart/services/cart.service.ts
@@ -2,6 +2,9 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { IShoppingCart } from '../cart';
 import { switchMap } from 'rxjs/operators';
+import { Firestore, collection, collectionData, doc } from '@angular/fire/firestore';
+import { ICartItem } from 'src/app/store/interfaces/cart-item';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -9,6 +12,7 @@ import { switchMap } from 'rxjs/operators';
 export class CartService {
   private _http: HttpClient = inject(HttpClient);
   private _apiUrl: string = 'http://localhost:3000';
+  private _firestore: Firestore = inject(Firestore);
 
   constructor() {}
 
@@ -22,6 +26,14 @@ export class CartService {
         }
       })
     );
+  }
+
+  public getUserShoppingCart(userEmail: string): Observable<ICartItem[]> {
+    const cartRef = doc(this._firestore, 'cart', userEmail);
+    const cartItemsRef = collection(cartRef, 'items');
+
+    const cartItems$ = collectionData(cartItemsRef, {idField: 'id'})
+    return cartItems$ as Observable<ICartItem[]>;
   }
 
   public getCartById(cartId: string) {
