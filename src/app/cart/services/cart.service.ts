746bc4ae8961d11a1b5b0dff8db8d8d209da493e
Jozuan Lopez <jozuanlopez@gmail.com>
2023-07-28 21:28:49 -0600

    Inserta un item en la collection shop-cart/id/items en firestore

--- src/app/cart/services/cart.service.ts
+++ src/app/cart/services/cart.service.ts
@@ -1,10 +1,12 @@
-import { Injectable, inject } from '@angular/core';
+import { Injectable, computed, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { IShoppingCart } from '../cart';
+import { IShoppingCart, ItemCartDto } from '../cart';
 import { switchMap } from 'rxjs/operators';
-import { Firestore, collection, collectionData, doc } from '@angular/fire/firestore';
+import { Firestore, addDoc, collection, collectionData, doc } from '@angular/fire/firestore';
 import { ICartItem } from 'src/app/store/interfaces/cart-item';
 import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/auth/services/auth.service';
+import { User } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root',
@@ -13,15 +15,27 @@ export class CartService {
   private _http: HttpClient = inject(HttpClient);
   private _apiUrl: string = 'http://localhost:3000';
   private _firestore: Firestore = inject(Firestore);
+  private _authService: AuthService = inject(AuthService);
+  private _user = computed<User | null | undefined>(() => this._authService.user());
 
   constructor() {}
 
   public getUserShoppingCart(userEmail: string): Observable<ICartItem[]> {
-    const cartRef = doc(this._firestore, 'cart', userEmail);
+    const cartRef = doc(this._firestore, 'shop-cart', userEmail);
     const cartItemsRef = collection(cartRef, 'items');
 
     const cartItems$ = collectionData(cartItemsRef, {idField: 'id'})
     return cartItems$ as Observable<ICartItem[]>;
+  }
+
+  public addItemToCart(item: ItemCartDto): void {
+    if(!this._user()) return;
+
+    const userEmail = this._user()?.email || '';
+    const cartRef = doc(this._firestore, 'shop-cart', userEmail);
+    const cartItemsRef = collection(cartRef, 'items');
+
+    addDoc(cartItemsRef, item);
   }
 
   public getCartById(cartId: string) {
