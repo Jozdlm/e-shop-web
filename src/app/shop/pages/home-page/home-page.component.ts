d71b1210c54635c4119da38cada44343f5512b01
Jozuan Lopez <jozuanlopez@gmail.com>
2023-09-04 21:42:49 -0600

    Define el header de la seccion de productos de la home-page

--- src/app/shop/pages/home-page/home-page.component.ts
+++ src/app/shop/pages/home-page/home-page.component.ts
@@ -4,12 +4,18 @@ import { ProductsService } from 'src/app/shop/services/products.service';
 import { ProductCardComponent } from 'src/app/shop/components/product-card/product-card.component';
 import { from } from 'rxjs';
 import { SearchInputComponent } from '../../components/search-input/search-input.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   standalone: true,
-  imports: [CommonModule, ProductCardComponent, SearchInputComponent],
+  imports: [
+    CommonModule,
+    ProductCardComponent,
+    SearchInputComponent,
+    RouterModule,
+  ],
 })
 export class HomePageComponent {
   private _productsService: ProductsService = inject(ProductsService);
