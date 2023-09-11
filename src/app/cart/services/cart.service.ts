4880071f775032f3a0aecf437925f2bbbdda478e
Jozuan Lopez <jozuanlopez@gmail.com>
2023-09-10 22:07:48 -0600

    Elimina la integracion con firebase y define un array estático para cart-items

--- src/app/cart/services/cart.service.ts
+++ src/app/cart/services/cart.service.ts
@@ -1,36 +1,16 @@
-import { Injectable, computed, inject } from '@angular/core';
-import { ItemCartDto } from '../cart';
-import { Firestore, addDoc, collection, collectionData, doc } from '@angular/fire/firestore';
-import { ICartItem } from 'src/app/cart/cart';
-import { Observable } from 'rxjs';
-import { AuthService } from 'src/app/auth/services/auth.service';
-import { User } from '@angular/fire/auth';
+import { Injectable } from '@angular/core';
+import { ICartItem, ItemCartDto } from '../cart';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
-  private _firestore: Firestore = inject(Firestore);
-  private _authService: AuthService = inject(AuthService);
-  private _user = computed<User | null | undefined>(() => this._authService.user());
+  private _cartItems: ICartItem[] = [];
 
   constructor() {}
 
-  public getUserShoppingCart(userEmail: string): Observable<ICartItem[]> {
-    const cartRef = doc(this._firestore, 'shop-cart', userEmail);
-    const cartItemsRef = collection(cartRef, 'items');
-
-    const cartItems$ = collectionData(cartItemsRef, {idField: 'id'})
-    return cartItems$ as Observable<ICartItem[]>;
-  }
-
   public addItemToCart(item: ItemCartDto): void {
-    if(!this._user()) return;
-
-    const userEmail = this._user()?.email || '';
-    const cartRef = doc(this._firestore, 'shop-cart', userEmail);
-    const cartItemsRef = collection(cartRef, 'items');
-
-    addDoc(cartItemsRef, item);
+    const newItem: ICartItem = { ...item, id: item.product_id.toString()};
+    this._cartItems.push(newItem);
   }
 }
