3ca6e893325483787831cf6b75016ec23fbc4dc8
Jozuan Lopez <jozuanlopez@gmail.com>
2023-09-13 21:14:07 -0600

    Define getter reactivo para la cantidad de items de carrito

--- src/app/cart/services/cart.service.ts
+++ src/app/cart/services/cart.service.ts
@@ -1,23 +1,29 @@
 import { Injectable } from '@angular/core';
 import { ICartItem, ItemCartDto } from '../cart';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
-  private items: ICartItem[] = [];
-  private _cartItems$ = new BehaviorSubject<ICartItem[]>(this.items);
+  private _items: ICartItem[] = [];
+  private _cartItems$ = new BehaviorSubject<ICartItem[]>(this._items);
 
   constructor() {}
 
-  public get cartItems(): Observable<ICartItem[]> {
+  public get cartItems$(): Observable<ICartItem[]> {
     return this._cartItems$.asObservable();
+  }
+
+  public get cartCount$(): Observable<number> {
+    return this.cartItems$.pipe(
+      map((items) => items.length)
+    );
   }
 
   public addItemToCart(item: ItemCartDto): void {
     const newItem: ICartItem = { ...item, id: item.product_id.toString() };
-    this.items.push(newItem);
-    this._cartItems$.next(this.items);
+    this._items = [...this._items, newItem];
+    this._cartItems$.next(this._items);
   }
 }
