b71e288c41a577562faecdabfd99ec604c4ac5cd
Jozuan Lopez <jozuanlopez@gmail.com>
2023-10-04 21:32:35 -0600

    Define los primeros campos del formulario de checkout

--- src/app/cart/pages/checkout-page/checkout-page.component.ts
+++ src/app/cart/pages/checkout-page/checkout-page.component.ts
@@ -4,7 +4,13 @@ import { CommonModule } from '@angular/common';
 @Component({
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './checkout-page.component.html'
+  templateUrl: './checkout-page.component.html',
+  styles: [`
+    .wrapper{
+      display: grid;
+      grid-template-columns: 1fr 394px;
+    }
+  `]
 })
 export class CheckoutPageComponent {
 
