7311463401502fee853735070180ad475cff9dcd
Jozuan Lopez <jozuanlopez@gmail.com>
2023-10-09 20:07:00 -0600

    Mejora la legibilidad del componente dropdown

--- src/app/common/components/dropdown/dropdown.component.ts
+++ src/app/common/components/dropdown/dropdown.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, HostListener, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
@@ -7,25 +7,21 @@ import { RouterModule } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterModule],
   template: `
-    <div
-      class="relative"
-      (mouseenter)="displayDropdown()"
-      (mouseleave)="hideDropdown()"
-    >
+    <div class="relative">
       <!-- Dropdown Toggle -->
       <button class="rounded p-2" [routerLink]="pageUrl">
         <ng-content select="#toggle-content"></ng-content>
       </button>
       <div
-        class="right-0 top-10 rounded border border-neutral-300 bg-white drop-shadow-lg z-20"
+        class="right-0 top-10 z-20 rounded border border-neutral-300 bg-white drop-shadow-lg"
+        [class.absolute]="showDropdown"
+        [class.hidden]="!showDropdown"
         [ngClass]="{
-          absolute: showDropdown,
-          hidden: !showDropdown,
           'w-52 py-1': isMenu,
           'w-80 p-4': !isMenu
         }"
       >
-        <!-- Contenido del Dropdown -->
+        <!-- Dropdown Body -->
         <ng-content select="#dropdown-content"></ng-content>
       </div>
     </div>
@@ -43,10 +39,12 @@ export class DropdownComponent {
 
   constructor() {}
 
+  @HostListener('mouseenter')
   public displayDropdown(): void {
     this.showDropdown = true;
   }
 
+  @HostListener('mouseleave')
   public hideDropdown(): void {
     this.showDropdown = false;
   }
