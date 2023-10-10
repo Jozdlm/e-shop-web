ad78075b6ff57bbba2266bfa721d4a2e2d53e04a
Jozuan Lopez <jozuanlopez@gmail.com>
2023-10-10 10:24:36 -0600

    Define el template y la lógica del componente chip

--- src/app/common/components/chip/chip.component.ts
+++ src/app/common/components/chip/chip.component.ts
@@ -1,18 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-chip',
+  selector: 'ui-chip',
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      chip works!
-    </p>
+    <div
+      class="mb-6 flex max-w-max items-center gap-x-1 rounded-full px-2 py-1"
+      [ngClass]="{
+        'border border-gray-300': (chipType == 'default'),
+        'text-green-900 bg-green-200': (chipType == 'success')
+      }"
+    >
+      <p class="text-xs font-medium">{{ label }}</p>
+    </div>
   `,
-  styles: [
-  ]
+  styles: [],
 })
 export class ChipComponent {
+  public chipType: string = 'default';
 
+  @Input()
+  public label: string = 'label';
+
+  @Input()
+  public set type(type: string) {
+    switch (type) {
+      case 'success':
+        this.chipType = 'success';
+        break;
+
+      default:
+        this.chipType = 'default';
+        break;
+    }
+  }
 }
