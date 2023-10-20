787d8ccf17aaea630a4803d52202ae37574b7f3d
Jozuan Lopez <jozuanlopez@gmail.com>
2023-10-19 21:55:09 -0600

    Define servicio con método get-categories para obtener las categorías

---
+++ src/app/shop/services/categories.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Category } from '../product';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CategoriesService {
+  private _apiUrl: string = 'http://localhost:3000/api';
+  private _http: HttpClient = inject(HttpClient);
+
+  constructor() {}
+
+  public getCategories(): Observable<Category[]> {
+    return this._http.get<Category[]>(`${this._apiUrl}/categories`);
+  }
+}
