c54ba8ad019b5a76da7bf23ef578cc55da951d64
Jozuan Lopez <jozuanlopez@gmail.com>
2023-10-20 20:53:38 -0600

    Genera el componente item-list en el modulo shop

---
+++ src/app/shop/pages/item-list/item-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-item-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './item-list.component.html',
+  styles: [
+  ]
+})
+export class ItemListComponent {
+
+}
