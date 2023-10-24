6a03b6d67cdd69f363c29390c6448c8fe5be5000
Jozuan Lopez <jozuanlopez@gmail.com>
2023-10-23 20:52:44 -0600

    Muestra los resultados utilizando el componente product-card

--- src/app/shop/pages/item-list/item-list.component.ts
+++ src/app/shop/pages/item-list/item-list.component.ts
@@ -2,13 +2,21 @@ import { Component, Input, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IProduct } from '../../product';
 import { ProductsService } from '../../services/products.service';
+import { ProductCardComponent } from '../../components/product-card/product-card.component';
 
 @Component({
   selector: 'app-item-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ProductCardComponent],
   templateUrl: './item-list.component.html',
-  styles: [],
+  styles: [`
+    .list-wrapper {
+      display: grid;
+      grid-template-columns: repeat(4, 1fr);
+      column-gap: 32px;
+      row-gap: 24px;
+    }
+  `],
 })
 export class ItemListComponent {
   private _productsService: ProductsService = inject(ProductsService);
