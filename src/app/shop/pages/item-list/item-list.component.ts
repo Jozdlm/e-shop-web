f4acf7df09e34c17d6438a96c33a857073402af4
Jozuan Lopez <jozuanlopez@gmail.com>
2023-10-24 21:20:19 -0600

    Maneja las subscripciones del componente

--- src/app/shop/pages/item-list/item-list.component.ts
+++ src/app/shop/pages/item-list/item-list.component.ts
@@ -1,30 +1,42 @@
-import { Component, Input, inject } from '@angular/core';
+import { Component, DestroyRef, Input, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IProduct } from '../../product';
 import { ProductsService } from '../../services/products.service';
 import { ProductCardComponent } from '../../components/product-card/product-card.component';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-item-list',
   standalone: true,
   imports: [CommonModule, ProductCardComponent],
   templateUrl: './item-list.component.html',
-  styles: [`
-    .list-wrapper {
-      display: grid;
-      grid-template-columns: 260px 1fr;
-      column-gap: 32px;
-    }
-  `],
+  styles: [
+    `
+      .list-wrapper {
+        display: grid;
+        grid-template-columns: 260px 1fr;
+        column-gap: 32px;
+      }
+    `,
+  ],
 })
 export class ItemListComponent {
-  private _productsService: ProductsService = inject(ProductsService);
+  private _productsService = inject(ProductsService);
+  private _subscriptions = new Subscription();
   public products: IProduct[] = [];
+
+  public constructor() {
+    inject(DestroyRef).onDestroy(() => {
+      this._subscriptions.unsubscribe();
+    });
+  }
 
   @Input()
   public set category(categorySlug: string) {
-    this._productsService
-      .getProductsByCategory(categorySlug)
-      .subscribe((arr: IProduct[]) => (this.products = arr));
+    this._subscriptions.add(
+      this._productsService
+        .getProductsByCategory(categorySlug)
+        .subscribe((arr: IProduct[]) => (this.products = arr))
+    );
   }
 }
