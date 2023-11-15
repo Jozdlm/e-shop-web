1518a4ac1938831ecc1c7d110269b95bd693fe98
Jozuan Lopez <jozuanlopez@gmail.com>
2023-11-14 20:04:58 -0600

    Define los iconos utilizados en el componente order-summary

--- src/app/cart/components/order-summary/order-summary.component.ts
+++ src/app/cart/components/order-summary/order-summary.component.ts
@@ -2,11 +2,19 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ButtonComponent } from 'src/app/common/components/button/button.component';
+import { LockClosedIconComponent } from 'src/app/shared/icons/lock-closed-icon/lock-closed-icon.component';
+import { ClipboardDocIconComponent } from 'src/app/shared/icons/clipboard-doc-icon/clipboard-doc-icon.component';
 
 @Component({
   selector: 'app-order-summary',
   standalone: true,
-  imports: [CommonModule, RouterModule, ButtonComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ButtonComponent,
+    LockClosedIconComponent,
+    ClipboardDocIconComponent
+  ],
   templateUrl: './order-summary.component.html',
 })
 export class OrderSummaryComponent {
