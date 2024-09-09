00640261b1106f751d74902043f63dbf51d527be
Jozuan Lopez <jozuanlopez@gmail.com>
2024-09-08 20:44:24 -0600

    Update angular to v18

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,7 @@
 import { ApplicationConfig } from '@angular/core';
 import { importProvidersFrom } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {
-  provideHttpClient,
-  withInterceptorsFromDi,
-} from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { environment } from 'src/environments/environment';
