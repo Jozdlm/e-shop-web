1c898436530b6cb8742a6e97588df18fff764cb6
Jozuan Lopez <jozuanlopez@gmail.com>
2025-03-17 23:38:58 -0600

    Refactor ProductsPage to use input signal and effect for category handling

--- src/app/pages/products.page.ts
+++ src/app/pages/products.page.ts
@@ -1,4 +1,4 @@
-import { Component, DestroyRef, Input, inject } from '@angular/core';
+import { Component, DestroyRef, effect, inject, input } from '@angular/core';
 import { IProduct } from '../features/products/product.types';
 import { ProductsService } from '../features/products/products.service';
 import { ProductCardComponent } from '../components/product-card/product-card.component';
@@ -8,21 +8,12 @@ import { CategoriesService } from '@app/features/products/categories.service';
 @Component({
   selector: 'app-item-list',
   imports: [ProductCardComponent],
-  styles: [
-    `
-      .list-wrapper {
-        display: grid;
-        grid-template-columns: 260px 1fr;
-        column-gap: 32px;
-      }
-    `,
-  ],
   template: `
     <div>
       <p>Resultados: {{ products.length }}</p>
     </div>
 
-    <div class="list-wrapper">
+    <div class="grid grid-cols-[260px_1fr] gap-8">
       <div>
         <p>Filters</p>
       </div>
@@ -39,29 +30,28 @@ export class ProductsPage {
   private readonly _categoryService = inject(CategoriesService);
   private _subscriptions = new Subscription();
   public products: IProduct[] = [];
+  public categorySlug = input.required<string>({
+    alias: 'category',
+  });
 
   public constructor() {
+    effect(() => {
+      this._subscriptions.add(
+        this._categoryService
+          .getCategoryBySlug(this.categorySlug())
+          .pipe(
+            switchMap((result) => {
+              if (result?.id) {
+                return this._productsService.getProductsByCategory(result.id);
+              }
+              return throwError(() => new Error('Category not found'));
+            }),
+          )
+          .subscribe((arr: IProduct[]) => (this.products = arr)),
+      );
+    });
     inject(DestroyRef).onDestroy(() => {
       this._subscriptions.unsubscribe();
     });
-  }
-
-  // TODO: Skipped for migration because:
-  //  Accessor inputs cannot be migrated as they are too complex.
-  @Input()
-  public set category(categorySlug: string) {
-    this._subscriptions.add(
-      this._categoryService
-        .getCategoryBySlug(categorySlug)
-        .pipe(
-          switchMap((result) => {
-            if (result?.id) {
-              return this._productsService.getProductsByCategory(result.id);
-            }
-            return throwError(() => new Error('Category not found'));
-          }),
-        )
-        .subscribe((arr: IProduct[]) => (this.products = arr)),
-    );
   }
 }
