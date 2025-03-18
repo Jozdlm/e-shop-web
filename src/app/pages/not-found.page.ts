cc97d61dde2d3fb7ec0a07df57b70479fbba8dd4
Jozuan Lopez <jozuanlopez@gmail.com>
2025-03-18 09:36:44 -0600

    Enhance NotFoundPage layout and messaging for better user experience

--- src/app/pages/not-found.page.ts
+++ src/app/pages/not-found.page.ts
@@ -5,9 +5,16 @@ import { Component } from '@angular/core';
   imports: [RouterModule],
   selector: 'app-404-page',
   template: `
-    <h1>Page not found</h1>
-    <p>Please review if you spell correctly the page url</p>
-    <a routerLink="">Regresar a la tienda</a>
+    <div
+      class="mx-auto text-center h-full flex flex-col justify-center items-center"
+    >
+      <h1 class="text-2xl font-medium mb-4">La página no fue encontrada</h1>
+      <p>
+        Lo sentimos, no encontramos la página que estabas buscando, por favor
+        verifica que la escribiste correctamente.
+      </p>
+      <a routerLink="" class="underline text-blue-900">Regresar a la tienda</a>
+    </div>
   `,
 })
 export class NotFoundPage {}
