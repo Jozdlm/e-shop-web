2d4cadcd147d2adc4e41b29836903b33830cc930
Jozuan Lopez <jozuanlopez@gmail.com>
2025-03-19 09:05:29 -0600

    Remove apiUrl from environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,6 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000',
   supabaseUrl: "https://grvggsbcrwpzcjvrfvhf.supabase.co",
   supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdydmdnc2JjcndwemNqdnJmdmhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTA4NTU0NTksImV4cCI6MjAwNjQzMTQ1OX0.KXtUaNjr2CIxW7iMmOh-cA4Ys05Y9zgiTkOCBoApiwM"
 };
